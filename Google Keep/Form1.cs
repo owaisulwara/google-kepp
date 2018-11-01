113d85b7b11713ac431ee3689ffc253732bf4f87
Agha Shayan <39364365+AghaShayan123@users.noreply.github.com>
2018-11-01 15:45:13 +0500

    Google Keep Desktop application

---
+++ Google Keep/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Google_Keep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("Google Keep", Application.ExecutablePath.ToString());
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("https://keep.google.com/u/0/");
+                webBrowser1.Navigate(queryaddress.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+            
+        }
+    }
+}
